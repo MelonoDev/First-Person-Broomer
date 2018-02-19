c8c01cef74ddd7089d8f23f89a47ea2cf66869bc
MelonoDev <36413982+MelonoDev@users.noreply.github.com>
2018-02-19 10:08:45 +0100

    Initial commit

---
+++ Assets/Scripts/BroomHitboxFollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomHitboxFollowPlayer : MonoBehaviour {
+
+
+
+	public Transform Character; // Target Object to follow
+	public float speed=0.5F; // Enemy speed
+	private Vector3 directionOfCharacter;
+	GameObject mainCamera = GameObject.FindWithTag("MainCamera");
+	private Vector3 positionInFront;
+
+
+	void Start() {
+		
+	}
+
+	void Update () {
+		positionInFront = mainCamera.transform.position;
+		directionOfCharacter = (positionInFront + new Vector3 (0, 0, 2)) - transform.position;
+		directionOfCharacter = directionOfCharacter.normalized;    // Get Direction to Move Towards
+		transform.Translate (directionOfCharacter * speed, Space.World);
+	}
+}
+
---
+++ Assets/Scripts/PlayerBroomSweep.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Add to player camera
+public class PlayerBroomSweep : MonoBehaviour {
+
+	public Rigidbody Sweep;
+	public float SweepTime;
+
+	void MakeSweep () {
+		Vector3 frontPosition = transform.TransformPoint (0, 0, 2);
+		Rigidbody sweepClone = (Rigidbody)Instantiate (Sweep, frontPosition, Sweep.transform.rotation);
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			MakeSweep ();
+		}
+	}
+}
